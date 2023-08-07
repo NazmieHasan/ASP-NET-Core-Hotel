449f6bfa33c2253166804bcbce1d54902df2d841
NazmieHasan <Nazi87@abv.bg>
2023-08-07 23:34:00 +0300

    change in the project architecture

--- ASP-NET-Core-Hotel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP_NET_Core_Hotel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
